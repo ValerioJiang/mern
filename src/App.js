a628afbab488a449310845f4c674916b62ed6c54
Valerio Jiang <jiangvalerio1998@gmail.com>
2021-06-11 14:06:17 +0200

    component init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
+
+import Navbar from './components/navbar.component'
+import ExcerciseList from './components/excercises-list.component'
+import EditExcercise from './components/edit-excercise.component'
+import CreateExcercise from './components/create-excercise.component'
+import CreateUser from './components/create-user.component'
 
 function App() {
   return (
-    <div className="container">
-      Hello world
-    </div>
+    <Router>
+      <Navbar>
+        <br/>
+        <Route path="/" exact component={ExcerciseList} />
+        <Route path="/edit/:id" component={EditExcercise} />
+        <Route path="/create" exact component={CreateExcercise} />
+        <Route path="/user" exact component={CreateUser} />
+
+      </Navbar>
+    </Router>
   );
 }
 
